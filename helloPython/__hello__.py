71a58e75e9ca9292dd76310602ee4d034a50b5ff
sunxiaoxiong <sunxiaoxiong@rtmap.com>
2021-11-18 18:48:24 +0800

    方法和函数

--- helloPython/__hello__.py
+++ helloPython/__hello__.py
@@ -1,12 +1,30 @@
 # -*- coding:utf-8 -*-
 # coding=utf-8
 
-print("hello python")
+# 多行注释，"""..."""
+"""
+1、可变类型，值可以改变：
+列表 list
+字典 dict
+2、不可变类型，值不可以改变：
+数值类型 int, long, bool, float
+字符串 str
+元组 tuple
+set  （没有value的字典）
+"""
 
+print("hello python")
+# '''...'''表示多行数据
+print ('''a 
+b
+c''')
+
+# 有了变量名和数值，不用指定类型，就能自动辨别类型
 num1 = 4
 num2 = 5.3
 result = num1 + num2
 print(result)
+# type() 查看变量的类型
 print (type(num1))
 print (type(num2))
 
@@ -18,13 +36,17 @@ print (keyword.kwlist)
 # print()会依次打印每个字符串，遇到逗号“,”会输出一个空格
 print"hello", "python"
 print '100 + 200 =', 100 + 200
-print ('hi,%s,you have %d' % ('python', 666))
+# %d 整数
+# %f 浮点数
+# %s 字符串
+# %x 十六进制整数
+print ("hi,%s,you have %d" % ('python', 666))
 
 # 输入的都作为字符串
-# shuru=raw_input("aa")
-# 输入的都作为表达式
-# shuru2=input(1+2)
+# shuru = raw_input("键盘输入:")
 # print (shuru)
+# 输入的都作为表达式
+# shuru2=input("键盘输入表达式:")
 # print (shuru2)
 
 # if elif else
@@ -36,6 +58,7 @@ elif age >= 6:
 else:
     print 'aa'
 
+# 输入的内容为字符串，要转换为数值类型再做比较
 # birth = input("birth:")
 # birth=int(birth)
 # if birth<2000:
@@ -82,6 +105,7 @@ while i3 < 10:
 # 字符串
 name = 'abcdef'
 print name[2]  # c
+# 左闭右开
 print name[1:3]  # bc
 print name[2:]  # cdef
 
@@ -90,12 +114,99 @@ print name.index('bc', 0, len(name))
 print name.count('bc', 0, len(name))
 name2 = name.replace('bc', 'zz')
 print name2
+print name.split("c")
 
-# 列表
+# 列表,索引下标从0开始,数据可以修改
 list = ['aa', 'bb', 'cc']
+print list[0]
 print len(list)
 
 a = [1, 2]
 b = [3, 4]
-a.append(b)
+a.append(b)  # [1, 2, [3, 4]]
+# extend可以将另一个集合中的元素逐一添加到列表中
+a.extend(b)  # [1, 2, [3, 4], 3, 4]
 print a
+
+# insert(index, object) 在指定位置index前插入元素object
+b.insert(1, 5)
+print b
+
+# del：根据下标进行删除，其实可以删除所有变量
+# pop：默认删除最后一个元素
+# remove：根据括号中元素的值进行删除第一个
+del a[0]
+print a
+a.pop()
+print a
+a.remove(3)
+print a
+
+# 排序
+c = [1, 4, 2, 3]
+c.sort()
+print c
+c.sort(reverse=True)
+print c
+
+chars = ['a', 'c', 'b', 'd']
+for value in chars:
+    print value
+for i, chr in enumerate(chars):
+    print i, chr
+
+# 元组 和list类似，但是元素不可以修改
+# 只有1个元素的tuple定义时必须加一个逗号
+tuplea = (1,)
+print tuplea
+# 元组元素指向不变
+tupleb = (1, 2, [3, 4])
+tupleb[2][0] = 3.1
+tupleb[2][1] = 4.1
+print tupleb
+
+# dict 字典，相当于是map
+dict1 = {"aa": 1, "bb": 2}
+print dict1["aa"]
+
+bb = dict1.get("b")
+print bb
+# 如果不存在该键，则会用默认的值
+bb = dict1.get("b", 3)
+print bb
+# 修改、新增元素值
+dict1["aa"] = 3
+print dict1
+# 删除元素值
+del dict1["aa"]
+print dict1
+# 删除字典
+# del dict1
+# print dict1
+# 清空字典
+dict1.clear()
+print dict1
+
+dict2 = {"aa": 1, "bb": 2, "cc": 3}
+keys = dict2.keys()
+print keys
+values = dict2.values()
+print values
+items = dict2.items()
+print items
+exists = dict2.has_key("aa")
+print exists
+# 遍历字典
+for key in dict2.keys():
+    print key, dict2[key]
+
+for value in dict2.values():
+    print value
+
+for item in dict2.items():
+    print item
+
+for key, value in dict2.items():
+    print key
+    print value
+    print "key=%s,value=%d" % (key, value)
--- helloPython/_function_.py
+++ helloPython/_function_.py
@@ -3,51 +3,81 @@
 
 def printInfo():
     print '定义函数'
+
+
 printInfo()
 
-def add2num(a,b):
-    c=a+b
+
+def add2num(a, b):
+    c = a + b
     print c
-add2num(2,3)
 
-def printInfo2(name,age=18):
-    print 'name',name
-    print 'age',age
+
+add2num(2, 3)
+
+
+def printInfo2(name, age=18):
+    print 'name', name
+    print 'age', age
+
+
 printInfo2('张三')
-printInfo2('李四',22)
+printInfo2('李四', 22)
 
-def fun(a,b,*args,**kwargs):
-    print 'a=',a
-    print 'b=',b
-    print 'args=',args
+
+def fun(a, b, *args, **kwargs):
+    print 'a=', a
+    print 'b=', b
+    print 'args=', args
     print 'kwargs='
-    for key,value in kwargs.items():
-        print key,'=',value
-fun(1,2,3,4,5,m=6,n=7,p=8)
+    for key, value in kwargs.items():
+        print key, '=', value
 
-def fun2(a,b):
-    return a+b
-c=fun2(1,2)
+
+fun(1, 2, 3, 4, 5, m=6, n=7, p=8)
+
+
+def fun2(a, b):
+    return a + b
+
+
+c = fun2(1, 2)
 print c
 
-def fun3(a,b):
-    jia=a+b
-    jian=a-b
-    return jia,jian
-jia1,jian1=fun3(3,2)
+
+# 函数返回值也可以返回多个值
+def fun3(a, b):
+    jia = a + b
+    jian = a - b
+    return jia, jian
+
+
+jia1, jian1 = fun3(3, 2)
 print jia1
 print jian1
 
-#全局变量
-a=200
+# 全局变量
+a = 200
+
+
+# 如果全局变量和局部变量名一样，则使用的是局部变量
 def test1():
-    a=30
-    print 'test1-1 %d'%a
-    a=40
-    print 'test1-2 %d'%a
+    a = 30
+    print 'test1-1 %d' % a
+    a = 40
+    print 'test1-2 %d' % a
+
+
+"""
+ 函数中修改全局变量，那么就需要使用global进行声明，否则出错
+1、如果全局变量是可变类型：所以在函数里面任意修改（值，引用）
+2、如果全局变量是不可变类型：在函数里面不能修改值，也不能修改引用，除非加上global才能修改引用。
+"""
 def test2():
     global a
-    a+=100
-    print 'test2 %d'%a
+    a += 100
+    print 'test2 %d' % a
+
+
 test1()
 test2()
